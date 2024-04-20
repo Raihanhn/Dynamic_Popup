671e1a0b4302dc661348af41c437513daa66711c
raihan <raihanhn20@gmail.com>
2024-04-21 04:51:27 +0600

    3rd popup portion added

---
+++ app/components/Modal3.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from "react";
+
+interface ModalProps {
+  isVisible: boolean;
+  onClose: () => void;
+  children: ReactNode;
+}
+
+export default function Modal3({ isVisible, onClose, children }: ModalProps) {
+  if (!isVisible) return null;
+
+  return (
+    <div className="flex items-center justify-center p-12 ">
+      <div className="  py-[10px]  fixed bottom-0 pl-[70px] pr-[70px]   lg:pl-[150px] lg:pr-[150px]  xl:pl-[250px] xl:pr-[250px] 2xl:pl-[350px] 2xl:pr-[350px] ">
+        {children}
+      </div>
+    </div>
+  );
+}
--- app/components/Popup.tsx
+++ app/components/Popup.tsx
@@ -4,13 +4,12 @@ import Modal from "./Modal";
 import { MdAddCircle } from "react-icons/md";
 import Modal2 from "./Modal2";
 import { HiMinusCircle } from "react-icons/hi";
+import Modal3 from "./Modal3";
 
 const Popup = () => {
   const [showModal, setShowModal] = useState(true);
   const [showModal2, setShowModal2] = useState(false);
-  const handleCloseModal = () => {
-    setShowModal(false);
-  };
+  const [showModal3, setShowModal3] = useState(false);
 
   return (
     <Fragment>
@@ -69,7 +68,7 @@ const Popup = () => {
           <div className="flex mt-[-135px]">
             <button
               className="text-[#73327E] text-xl  "
-              onClick={() => setShowModal2(false)}
+              onClick={() => setShowModal3(true)}
             >
               <MdAddCircle className=" w-6 h-6  " />
             </button>
@@ -82,6 +81,89 @@ const Popup = () => {
           </div>
         </div>
       </Modal2>
+
+      <Modal3 isVisible={showModal3} onClose={() => setShowModal3(false)}>
+        <div className="  pl-[140px] pt-[40px] pb-[70px] pr-[140px] bg-[#EDEDED]  ">
+          <div className=" flex justify-end items-end ">
+            <button
+              className="text-[#73327E] text-xl    "
+              onClick={() => setShowModal3(false)}
+            >
+              <HiMinusCircle className=" w-6 h-6 " />
+            </button>
+          </div>
+          <div className=" leading-[26px] ">
+            <p className="text-[12px] md:text-[12px] lg:text-[16px] xl:text-[18px] font-semibold  text-[#73327E]">
+              {" "}
+              WHAT IS CYTALUX®?
+            </p>
+            <p>
+              CYTALUX is an FDA approved prescription medication that is given
+              prior to surgery to adult patients who have ovarian cancer or
+              known or suspected cancer in the lung. It helps surgeons visualize
+              ovarian and lung cancer lesions during surgery.
+            </p>
+            <p className="text-[12px] md:text-[12px] lg:text-[16px] xl:text-[18px] font-semibold  text-[#73327E]">
+              IMPORTANT SAFETY INFORMATION
+            </p>
+            <p>
+              <b> Infusion-Related Reactions</b>
+              <br />
+              Adverse reactions including nausea, vomiting, abdominal pain,
+              flushing, indigestion, chest discomfort, and allergic reactions
+              were reported during the administration of CYTALUX. Your doctor
+              may treat you with antihistamines and/or anti-nausea medication.
+            </p>
+            <p>
+              <b> Risk of Misinterpretation</b>
+              <br />
+              Errors may occur with the use of CYTALUX. Sometimes cells may
+              light up even if they are not cancerous or those that are
+              cancerous may not light up. Also, non-cancerous cells from other
+              areas may light up, such as areas of the bowel, kidneys, lymph
+              nodes, lungs, and inflamed tissue.
+            </p>
+            <p>
+              <b>Pregnancy</b>
+              <br />
+              CYTALUX may cause fetal harm when administered to a pregnant
+              woman. There are no available human data to evaluate for a
+              drug-associated risk of major birth defects, miscarriage, or other
+              adverse maternal or fetal outcomes. Contact your healthcare
+              provider with a known or suspected pregnancy.
+            </p>
+            <p>
+              <b>Folate Supplementation Usage</b>
+              <br />
+              Folic acid may reduce the detection of cancerous tissue with
+              CYTALUX. Patients should stop taking folate, folic acid, or
+              folate-containing supplements 48 hours before administration of
+              CYTALUX.
+            </p>
+            <p>
+              <b> Adverse Reactions</b>
+              <br />
+              The most common side effects of CYTALUX reported in clinical
+              trials were nausea (13%), vomiting (5%), abdominal pain (2%),
+              flushing (2%), other infusion-related reactions (2%), allergic
+              reaction (2%), elevation in blood pressure (1%), indigestion (1%),
+              and chest discomfort (1%) during administration.
+            </p>
+            <p>
+              Tell your healthcare provider if you have any side effect that
+              bothers you or that does not go away. These are not all the
+              possible side effects of CYTALUX. For more information, ask your
+              healthcare provider.
+            </p>
+            <p>
+              Call your doctor for medical advice about side effects. You may
+              report side effects to On Target Laboratories at 1-844-434-9333 or
+              FDA at 1-800-FDA-1088 or www.fda.gov/medwatch.
+            </p>
+            <p>See full Prescribing Information for more details</p>
+          </div>
+        </div>
+      </Modal3>
     </Fragment>
   );
 };
