2f2782d3e38d4576708a782b920eba7b84247f6a
raihan <raihanhn20@gmail.com>
2024-04-23 04:29:57 +0600

    adding newsticker

---
+++ app/components/NewsTicker/NewsTicker.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./newsticker.css";
+
+const NewsTicker = () => {
+  return (
+    <div className="news-container">
+      <div className="title">Breaking News</div>
+    </div>
+  );
+};
+
+export default NewsTicker;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Popup from "./components/Popup";
+import NewsTicker from "./components/NewsTicker/NewsTicker";
 
 export default function Home() {
   return (
     <div className="mt-[6rem] ">
+      <NewsTicker />
       <h1>Hello World</h1>
       <Popup />
     </div>
