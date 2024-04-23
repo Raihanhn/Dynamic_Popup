362a95191dcec450e79856108fbb4de042bba4fd
raihan <raihanhn20@gmail.com>
2024-04-23 06:46:44 +0600

    newsticker completed

--- app/components/NewsTicker/NewsTicker.tsx
+++ app/components/NewsTicker/NewsTicker.tsx
@@ -3,8 +3,21 @@ import "./newsticker.css";
 
 const NewsTicker = () => {
   return (
-    <div className="news-container">
-      <div className="title">Breaking News</div>
+    <div className="mt-14">
+      <div className="news-container">
+        <div className="title">Breaking News</div>
+        <ul>
+          <li>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore,
+            ipsa!
+          </li>
+          <li>Lorem ipsum dolor sit</li>
+          <li>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore,
+            ipsa!
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
