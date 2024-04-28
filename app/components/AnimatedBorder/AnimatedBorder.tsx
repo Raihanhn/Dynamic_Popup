37d25ef925a75c1aee8fbf80089d8c201ed36201
raihan <raihanhn20@gmail.com>
2024-04-28 11:03:08 +0600

    creating animated border

---
+++ app/components/AnimatedBorder/AnimatedBorder.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./animatedborder.css";
+const AnimatedBorder = () => {
+  return (
+    <div>
+      <div className="animated-card"></div>
+    </div>
+  );
+};
+
+export default AnimatedBorder;
--- app/components/NewsTicker2/Modem/PopupModem.tsx
+++ app/components/NewsTicker2/Modem/PopupModem.tsx
@@ -32,12 +32,86 @@ const PopupModem = () => {
               </div>
             ) : currentState === "model3" ? (
               <div className="flex justify-between ">
-                <div className="">Hello</div>
+                <div className=" leading-[26px] ">
+                  {/* <p className="text-[12px] md:text-[12px] lg:text-[16px] xl:text-[18px] font-semibold  text-[#73327E]"> */}
+                  <p className=" font-semibold  text-[#73327E]">
+                    {" "}
+                    WHAT IS CYTALUX®?
+                  </p>
+                  <p>
+                    CYTALUX is an FDA approved prescription medication that is
+                    given prior to surgery to adult patients who have ovarian
+                    cancer or known or suspected cancer in the lung. It helps
+                    surgeons visualize ovarian and lung cancer lesions during
+                    surgery.
+                  </p>
+                  <p className=" font-semibold  text-[#73327E]">
+                    IMPORTANT SAFETY INFORMATION
+                  </p>
+                  <p>
+                    <b> Infusion-Related Reactions</b>
+                    <br />
+                    Adverse reactions including nausea, vomiting, abdominal
+                    pain, flushing, indigestion, chest discomfort, and allergic
+                    reactions were reported during the administration of
+                    CYTALUX. Your doctor may treat you with antihistamines
+                    and/or anti-nausea medication.
+                  </p>
+                  <p>
+                    <b> Risk of Misinterpretation</b>
+                    <br />
+                    Errors may occur with the use of CYTALUX. Sometimes cells
+                    may light up even if they are not cancerous or those that
+                    are cancerous may not light up. Also, non-cancerous cells
+                    from other areas may light up, such as areas of the bowel,
+                    kidneys, lymph nodes, lungs, and inflamed tissue.
+                  </p>
+                  <p>
+                    <b>Pregnancy</b>
+                    <br />
+                    CYTALUX may cause fetal harm when administered to a pregnant
+                    woman. There are no available human data to evaluate for a
+                    drug-associated risk of major birth defects, miscarriage, or
+                    other adverse maternal or fetal outcomes. Contact your
+                    healthcare provider with a known or suspected pregnancy.
+                  </p>
+                  <p>
+                    <b>Folate Supplementation Usage</b>
+                    <br />
+                    Folic acid may reduce the detection of cancerous tissue with
+                    CYTALUX. Patients should stop taking folate, folic acid, or
+                    folate-containing supplements 48 hours before administration
+                    of CYTALUX.
+                  </p>
+                  <p>
+                    <b> Adverse Reactions</b>
+                    <br />
+                    The most common side effects of CYTALUX reported in clinical
+                    trials were nausea (13%), vomiting (5%), abdominal pain
+                    (2%), flushing (2%), other infusion-related reactions (2%),
+                    allergic reaction (2%), elevation in blood pressure (1%),
+                    indigestion (1%), and chest discomfort (1%) during
+                    administration.
+                  </p>
+                  <p>
+                    Tell your healthcare provider if you have any side effect
+                    that bothers you or that does not go away. These are not all
+                    the possible side effects of CYTALUX. For more information,
+                    ask your healthcare provider.
+                  </p>
+                  <p>
+                    Call your doctor for medical advice about side effects. You
+                    may report side effects to On Target Laboratories at
+                    1-844-434-9333 or FDA at 1-800-FDA-1088 or
+                    www.fda.gov/medwatch.
+                  </p>
+                  <p>See full Prescribing Information for more details</p>
+                </div>
                 <button
                   className="text-[#73327E] text-xl font-semibold "
                   onClick={() => setCurrentState("Sign Up")}
                 >
-                  <MdAddCircle className=" w-6 h-6  " />
+                  <HiMinusCircle className=" w-6 h-6 " />
                 </button>
               </div>
             ) : (
